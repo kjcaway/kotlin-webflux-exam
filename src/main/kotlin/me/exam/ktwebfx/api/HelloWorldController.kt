213a52bbbc7ffb8e9b2efb1be3bb8357a536885c
DESKTOP-CN2C0RT\jonchans <whdckstngh@naver.com>
2023-11-06 23:37:56 +0900

    **ADD -redis ops sample

--- src/main/kotlin/me/exam/ktwebfx/api/HelloWorldController.kt
+++ src/main/kotlin/me/exam/ktwebfx/api/HelloWorldController.kt
@@ -2,9 +2,13 @@ package me.exam.ktwebfx.api
 
 import me.exam.ktwebfx.base.ApiResult
 import me.exam.ktwebfx.config.JsonPropertyConfig
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
@@ -23,5 +27,36 @@ class HelloWorldController(
     fun getJsonEnv(): Mono<ApiResult<*>> {
 
         return Mono.just(ApiResult.ok(jsonPropertyConfig.secretKey))
+    }
+
+    @GetMapping("/redis")
+    fun getRedis(@RequestParam key: String, @RequestParam field: String): Mono<ApiResult<*>> {
+        return helloWorldService.getHashValue(key, field)
+                .map {
+                    ApiResult.ok(it)
+                }
+    }
+
+    @PostMapping("/redis")
+    fun postRedis(@RequestBody map: Map<*, *>): Mono<ApiResult<*>> {
+        return helloWorldService.setHash(map["key"] as String, map["field"] as String, map["value"] as String)
+                .map {
+                    ApiResult.ok()
+                }
+    }
+
+    @GetMapping("/redis/all")
+    fun getRedisAll(@RequestParam type: String, @RequestParam size: Long): Mono<ApiResult<*>> {
+        return helloWorldService.getKeys(type, size)
+                .collectList()
+                .map {
+                    ApiResult.ok(it)
+                }
+    }
+
+    @DeleteMapping("/redis/all")
+    fun deleteRedisAll(): Mono<ApiResult<*>> {
+        return helloWorldService.removeAll()
+                .then(Mono.fromCallable { ApiResult.ok() })
     }
 }
--- src/main/kotlin/me/exam/ktwebfx/api/HelloWorldService.kt
+++ src/main/kotlin/me/exam/ktwebfx/api/HelloWorldService.kt
@@ -1,8 +1,71 @@
 package me.exam.ktwebfx.api
 
+import me.exam.ktwebfx.error.CustomErrorCode
+import me.exam.ktwebfx.error.CustomException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.core.ScanOptions
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.switchIfEmpty
 
 @Service
-class HelloWorldService {
+class HelloWorldService(
+        val redisTemplate: ReactiveRedisTemplate<String, Any>,
+        val redisOperations: ReactiveRedisOperations<String, Any>
+) {
+    private val logger: Logger = LoggerFactory.getLogger(HelloWorldService::class.java)
 
+    private final val REDIS_PREFIX = "ktwebfx"
+    private final val TYPE_HASH = "hash"
+
+    fun getKeys(type: String, size: Long): Flux<String> {
+        val options = ScanOptions.scanOptions()
+                .match("${REDIS_PREFIX}::${type}::*")
+                .count(size)
+                .build()
+
+        return redisOperations.scan(options)
+    }
+
+    fun setHash(key: String, field: String, value: String): Mono<Boolean> {
+        val hashKey = "${REDIS_PREFIX}::${TYPE_HASH}::${key}"
+        return redisTemplate.opsForHash<String, String>().put(hashKey, field, value)
+                .doOnSuccess { logger.info("set hash $key $field $value") }
+                .doOnError { logger.error("failed to set hash $key $field $value", it) }
+    }
+
+    fun getHashValue(key: String, field: String): Mono<String?> {
+        val hashKey = "${REDIS_PREFIX}::${TYPE_HASH}::${key}"
+
+        return redisTemplate.opsForHash<String, String>().get(hashKey, field)
+                .switchIfEmpty {
+                    Mono.error(CustomException(CustomErrorCode.NOT_FOUND))
+                }
+                .mapNotNull { value ->
+                    value
+                }
+    }
+
+    fun removeHashField(key: String, field: String): Mono<Void> {
+        val hashKey = "${REDIS_PREFIX}::${TYPE_HASH}::${key}"
+
+        return redisTemplate.opsForHash<String, String>().remove(hashKey, field)
+                .doOnSuccess { logger.info("delete hash $key $field") }
+                .doOnError { logger.error("failed to delete hash $key $field", it) }
+                .then()
+    }
+
+    fun removeAll(): Mono<Void> {
+        return redisOperations.keys("${REDIS_PREFIX}*")
+                .flatMap { key ->
+                    redisOperations.delete(key)
+                            .doOnSuccess { logger.info("delete $key") }
+                            .doOnError { logger.error("failed to delete $key") }
+                }
+                .then()
+    }
 }
